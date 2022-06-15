fd663554c1b9ad4e8eb0b5511523cad2c029cfcb
Zokirkhon <zokirxonkotibxanov@gmail.com>
2022-06-15 19:50:56 +0900

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,8 @@ import Navbar from "./Navbar";
 const Layout = ({children}) => {
     return ( 
         <div className="content">
+             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5131083334106159"
+     crossorigin="anonymous"></script>
             <Navbar />
             {children}
             <Footer />
